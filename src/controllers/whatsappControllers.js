f1067cff4889538e2733f24503098b6c0c766fbb
STEVE10MR <jm2019063316@virtual.upt.pe>
2023-03-31 15:53:39 -0500

    Update whatsappControllers.js

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,6 +1,23 @@
 const verifyToken = (req,res)=>{
+    const verifyToken = (req,res)=>{
 
-    res.send("Hola receivedMessage")}
+        try{
+            let accessToken = "vFgAqJlRmSjOwPzXnEiKcYbHtUyDx"
+            const token = req.query["hub.verify_token"]
+            let challenge = req.body["hub.challenge"]
+    
+            if(challenge != null && token != null && token== accessToken){
+                res.send(challenges)
+            }
+            else{
+                res.status(400).send()
+            }
+        }
+        catch(err){
+            res.status(400).send()
+        }
+    }
+}
 const receivedMessage = (req,res)=>{
     res.send("Hola receivedMessage")}
 
