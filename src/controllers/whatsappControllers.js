22776d9e88ce65e8bcf40e1660155b9a9d531fa0
STEVE10MR <jm2019063316@virtual.upt.pe>
2023-03-31 15:55:28 -0500

    Update whatsappControllers.js

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -4,7 +4,7 @@ const verifyToken = (req,res)=>{
         try{
             let accessToken = "vFgAqJlRmSjOwPzXnEiKcYbHtUyDx"
             const token = req.query["hub.verify_token"]
-            let challenge = req.body["hub.challenge"]
+            let challenge = req.query["hub.challenge"]
     
             if(challenge != null && token != null && token== accessToken){
                 res.send(challenges)
